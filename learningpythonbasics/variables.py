9102a999a16cda8e3db7a50f58a9c99cb0012f9d
shittm06 <shittum06@gmail.com>
2024-11-19 15:57:59 -0500

    Create a simple variable lesson with int type, string and float types

---
+++ learningpythonbasics/variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+x = 3              # a whole number                   
+f = 3.1415926      # a floating point number              
+name = "Python"    # a string
+version = '3.13'
+
+print(x)
+print(f)
+print(name)
+
+combination = name + " " + version
+print(combination)
+
+sum = f + f
+print(sum)
